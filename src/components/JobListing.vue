bc2d489cd0bcec4769d89b0ff319087eff01ac64
Rab-son <rabsonsayendajnr@gmail.com>
2025-04-11 02:03:00 +0200

    feat: view all jobs button

--- src/components/JobListing.vue
+++ src/components/JobListing.vue
@@ -29,7 +29,7 @@ const props = defineProps({
           {{ job.location }}
         </div>
         <a
-          href="job.html"
+          :href="'/jobs/' + job.id"
           class="h-[36px] bg-green-500 hover:bg-green-600 text-white px-4 py-2 rounded-lg text-center text-sm"
         >
           Read More
--- src/components/JobListings.vue
+++ src/components/JobListings.vue
@@ -1,7 +1,15 @@
 <script setup>
 import JobListing from './JobListing.vue';
 import jobData from '@/jobs.json';
-import { ref } from 'vue';
+import { ref, defineProps } from 'vue';
+
+defineProps({
+    limit: Number,
+    showButton: {
+        type: Boolean,
+        default: false,
+    },
+})
 
 const jobs = ref(jobData.jobs);
 
@@ -12,8 +20,15 @@ const jobs = ref(jobData.jobs);
         <h2 class="text-3xl font-bold text-green-500 mb-6 text-center">Browse jobs</h2>
   
         <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
-          <JobListing v-for="job in jobs" :key="job.id" :job="job" />
+          <JobListing v-for="job in jobs.slice(0, limit || jobs.length)" :key="job.id" :job="job" />
         </div>
       </div>
+    </section>
+    <section v-if="showButton" class="m-auto max-w-lg my-10 px-6">
+      <a
+        href="/jobs"
+        class="block bg-black text-white text-center py-4 px-6 rounded-xl hover:bg-gray-700"
+        >View All Jobs</a
+      >
     </section>
   </template>
